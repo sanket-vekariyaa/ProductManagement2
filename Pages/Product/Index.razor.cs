80a60dc00a715d56547153c084c1557be9934d42
Avni <Avni@DESKTOP-EBJT3NE>
2025-01-01 11:20:08 +0530

    changed the structure

--- Pages/Product/Index.razor.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace ProductManagement.Components.Product
-{
-    public partial class Index
-    {
-        // _process.GetPage<Product>(pageData)
-        //  _process.Get<Product>(id ?? 0)
-    }
-}
